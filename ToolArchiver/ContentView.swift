8d9787e8011a71f0cb7f9455c96f00e42e9f39c1
Samanthagl19 <114088080+Samanthagl19@users.noreply.github.com>
2023-03-23 15:11:40 -0500

    Help

--- ToolArchiver/ContentView.swift
+++ ToolArchiver/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    
+    @State var IsLoginOn = false
+
     var body: some View {
-        Text("ENSAB")
-        .foregroundColor(.blue)
-        .bold()
-    }}
+        NavigationView{
+            ZStack{
+                     Image("icon-logo")
+                        .resizable()
+                         .scaledToFit()
+                         .clipShape(Circle())
+                         .overlay(Circle().stroke(Color(red: 39 / 255, green: 113 / 255, blue: 233 / 255), lineWidth: 5))
+                         .shadow(radius: 20)
+                         
+                     }
+        } //EOT Navigation
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
